fe826c728457ef979916596f0efff99c2473d914
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 15:52:12 +0500

    space craft

---
+++ spacecraft.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "";
+  cout <<"       /\\       "<<endl;
+  cout <<"    __/~~\\__" <<endl;
+  cout <<"  /   |  |   \\"<<endl;
+  cout <<" =====.===.===="<<endl;
+  cout <<"      |||||";
+  return 0;
+}
