dda77f1a774af1453bdb202f11bead098dced2e8
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 22:46:53 +0500

    gun

---
+++ gun.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+ cout<< "       +--^--------,--------,-------,--------,-----^--,     " << endl;    
+ cout << "        | ||||||||||     '----------'          |          o   "<<endl;                         
+ cout<<  "      '+-------------------------------------^----------|       "<<endl;
+ cout << "          ,------------,-----------,----------------------'        "<<endl;
+ cout << "          / XXXXXXXXX /'|          /'                         "<<endl;
+ cout << "         / XXXXXXXXX /  |         /'                       "<<endl;
+ cout << "        / XXXXXXXXX /                                             "<<endl;
+ cout << "       / XXXXXXXXX /'__________'                           "<<endl;
+ cout << "      / XXXXXXXXX /                                       "<<endl;
+ cout << "     / XXXXXXXXX /                                        "<<endl;
+ cout << "    (___________(                                                  "<<endl;
+ cout << "     '_________'                                           "<<endl;
+               
+
+  return 0;
+}
