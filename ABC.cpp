44006735af6e6963527d28f3fba84c4d15f38148
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 16:10:43 +0500

    ABC

---
+++ ABC.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        cout << "  .........         "<<endl;
+	cout << "  .........       "<<endl;
+	cout << "  ...   ...       "<<endl;
+	cout << "  ...   ...        "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "  .........            "<<endl;
+	cout << "  ...   ...           "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...   ...               "<<endl;
+cout<< ""<<endl;
+system("color 23");
+        cout << "  ...   ...         "<<endl;
+	cout << "  ...   ...             "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...  ....              "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "  .........            "<<endl;
+	cout << "  ...   ...           "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...   ...               "<<endl;
+cout<< ""<<endl;
+system("color 23");
+        cout << "  .........         "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "        ...            "<<endl;
+	cout << "       ...             "<<endl;
+	cout << "      ...            "<<endl;
+	cout << "     ...           "<<endl;
+	cout << "   ...            "<<endl;
+	cout << "  .........               "<<endl;
+        cout << "  .........               "<<endl;
+cout<< ""<<endl;
+system("color 23");
+        cout << "  ...   ...         "<<endl;
+	cout << "  ...   ...             "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...  ....              "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "  .........            "<<endl;
+	cout << "  ...   ...           "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...   ...               "<<endl;
+cout<< ""<<endl;
+system("color 23");
+ 	cout << "  .........         "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  ...              "<<endl;
+	cout << "  .........             "<<endl;
+	cout << "  .........            "<<endl;
+	cout << "        ...           "<<endl;
+	cout << "  ...   ...            "<<endl;
+	cout << "  .........               "<<endl;
+	cout << "  .........               "<<endl;
+
+system("color 23");
+
+  return 0;
+}
