612daae796231a32853b57fd5041aebd11ba21a5
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 22:29:45 +0500

    cow

---
+++ cow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout << "  -------------------------------------                                     "<<endl;
+ cout << "       o    ^__^                                "<<endl;
+ cout << "        o    (oo)\\___________                           "<<endl;
+ cout << "             (__)\\           )\\/\\                         "<<endl;
+ cout << "                  ||-------w |                     "<<endl;
+ cout << "                  ||        ||                     "<<endl;
+
+
+
+  return 0;
+}
