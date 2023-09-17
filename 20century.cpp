1b7cba643139e8f4ca66e0c39073d5dab1d87ae3
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 22:20:23 +0500

    20century

---
+++ 20century.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "                 ....    ....                                     "<<endl;
+ cout << "               ..   .. ..  ..                        "<<endl;
+ cout << "                   ..  ..  ..                                "<<endl;
+ cout << "                  ..   ..  ..                              "<<endl;
+ cout << "  .....        ......   ....   th               .....  "<<endl;
+ cout << " ......       ..................                 ......          "<<endl;
+ cout << "  .....           century                        .....         "<<endl;
+ cout << "   ....       ..................               .....            "<<endl;
+ cout << "    ....     ...... ....   .   .  ......      ...            "<<endl;
+ cout << "     ...       ..   ..      . .     ..       ...               "<<endl;
+ cout << "      __|__    ..   ....     .      ..     __|__         "<<endl;
+ cout << "     |     |   ..   ..      . .     ..     |    |                  "<<endl;
+ cout << "  ...|.....|.  ..   ....   .   .    ..  ...|....|....         "<<endl;
+ system("color 19");
+  return 0;
+}
