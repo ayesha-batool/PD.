b8ab2f888b051fd849503b84bf5423e3f86c0d24
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 22:37:34 +0500

    teacup

---
+++ teacup.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ cout << "                       8    8   8                "<<endl;
+ cout << "                         8   8   8                "<<endl;
+ cout << "                          8   8   8               "<<endl;
+ cout << "                         8   8   8                "<<endl;
+ cout << "                        8   8   8                 "<<endl;
+ cout << "                       8   8   8                 "<<endl;
+ cout << "                      8   8   8                  "<<endl;
+ cout << "                     8   8   8                 "<<endl;
+ cout << "                      8   8   8                 "<<endl;
+ cout << "                       8   8   8                        "<<endl;
+ cout << "                        8   8   8                       "<<endl;
+ cout << "                         8   8   8                "<<endl;
+ cout << "            888888888888888888888888888888888            "<<endl;
+ cout << "             8                            .8                     "<<endl;
+ cout << "              8.                          8.         "<<endl;
+ cout << "               8.                        8.     "<<endl;
+ cout << "                8.                      8.             "<<endl;
+ cout << "                 8.                    8.              "<<endl;
+ cout << "                  888888888888888888888                           "<<endl;
+
+
+  return 0;
+}
