633944055be8fe86ae0026dfbf3db8ad7a0ee367
ayesha-batool <batoolayesha123456@gmail.com>
2023-09-17 22:51:50 +0500

    cartoon

---
+++ cartoon.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "         '.-.           _____'                              "<<endl;
+ cout << "           '..!_......_/'      '                                "<<endl;
+ cout << "            !            /'. _'  '                      "<<endl;
+ cout << "             / ()    ()  !     .' '_._                    "<<endl;
+ cout << "            |)          () ! /      .'                       "<<endl;
+ cout << "             |   _'_      . ;     _                         "<<endl;
+ cout << "             ;.__        ''       <                          "<<endl;
+ cout << "            /         / ' |     > |                      "<<endl;
+ cout << "           (_/'      (_/  |  ._.'  _'                             "<<endl;
+ cout << "             !        |   .;| .<'                             "<<endl;
+ cout << "             !        ,    | _'                              "<<endl;
+ cout << "             !        !    ;                            "<<endl;
+ cout << "             >         !    /                              "<<endl;
+ cout << "             (_,-''> .' . .'                                     "<<endl;
+ cout << "               (_,'                                   "<<endl;
+
+
+
+  return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
